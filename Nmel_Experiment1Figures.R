fb6f44b1ef905fe5cf707a980ab623c7510cc560
Mallory Hagadorn <malloryhagadorn@gmail.com>
2021-02-10 13:47:37 -0700

    Updated figure scripts for revised submission
    Scripts for figures were updated based on pdfs submitted to the journal during revision.

--- Nmel_Experiment1Figures.R
+++ Nmel_Experiment1Figures.R
@@ -47,53 +47,56 @@ addline_format <- function(x,...){
 
 
 #Assigning specific sizes, changes to figures will reflect changes made here. Keep consistent with Exp.2 figures
-generalsize <- 18
-legendsize <- 13
-lettersize <- 5.2
+#scaled by 0.68
+generalsize <- 12
+legendsize <- 7
+wordsize <- 8
+lettersize <- 3
+dotsize <- 1.8
+
 
 
 
 #GENERATE FIGURE FOR NEUROPIL, KCS, AND ALS
-par(mar=c(0,2,4,6)) #(bottom, left, top, right)
+par(mar=c(0,0,0,0)) #(bottom, left, top, right)
 NKCAL_plot <- ggplot(melt_NKCAL, aes(x=variable, y=value, fill=factor(code))) +
-  geom_boxplot(outlier.shape = NA, color="black") +
-  scale_fill_manual(values = c("white", "gray70", "gray38"), labels=c("Newly-Emerged", "Lab-Reared", "Nesting")) +
-  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.2, seed=667), size=3.1, stroke=.7) +
-  geom_point(aes(color=factor(code)), shape=16, position=position_jitterdodge(jitter.width=.2, seed=667), alpha=.7, size=3) +
-  scale_color_manual(values=c("NewEmerge"="mediumblue", "LabReared"="violetred4","Nesting"="darkorange"), labels=c("Newly-Emerged", "Lab-Reared", "Nesting"), ) +
+  geom_boxplot(outlier.shape = NA, color="black",lwd=.3) +
+  scale_fill_manual(values = c("white", "gray45", "gray20"), labels=c("Newly-emerged", "Lab-reared", "Nesting")) +
+  geom_point(aes(color=factor(code)), shape=16, position=position_jitterdodge(jitter.width=.2, seed=667), alpha=.75, size=dotsize) +
+  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.2, seed=667), size=dotsize+.01, stroke=.3) +
+  scale_color_manual(values=c("NewEmerge"="mediumblue", "LabReared"="violetred4","Nesting"="darkorange"), labels=c("Newly-emerged", "Lab-reared", "Nesting"), ) +
   
   #Significance annotations from post-hoc results
   annotate(geom="text", x=1-0.25, y=max(NKCAL[NKCAL$code=="NewEmerge", 3]) +.006, label="a", color="black", size = lettersize) + #NE
   annotate(geom="text", x=1, y=max(NKCAL[NKCAL$code=="LabReared", 3]) +.006, label="a", color="black", size = lettersize) + #LR
   annotate(geom="text", x=1+0.25, y=max(NKCAL[NKCAL$code=="Nesting", 3]) +.006, label="b", color="black", size = lettersize) + #REP
-  ggtitle("(a)") +
+  ggtitle("A") +
   scale_y_continuous(name = "Volume Relative to Whole Brain", limits = c(0.03,.181), breaks=seq(0.03,.18, .03)) +
   scale_x_discrete(name = "", breaks=unique(melt_NKCAL$variable), labels=addline_format(c("MB Neuropil", "Kenyon Cells", "Antennal Lobes"))) +
-  theme(text = element_text(color="black", size = generalsize),
-        axis.title = element_text(color="black"), 
-        axis.text.x = element_text(color="black", size = generalsize), 
-        axis.text.y = element_text(color="black", size = generalsize, margin = margin(l = 10, r=5)),
+  theme(text = element_text(color="black", size = generalsize, family="sans"),
+        axis.title = element_text(color="black", size=9), 
+        axis.text.x = element_text(color="black", size = wordsize, margin = margin(l=0, r=0, t=2, b=-10)), 
+        axis.text.y = element_text(color="black", size = wordsize, margin = margin(l = 2, r=2)),
         axis.ticks.margin=unit(0,'cm'),
         axis.ticks.length = unit (.2,"cm"),
+        axis.ticks = element_line(color="black", size=.3),
         legend.title = element_blank(),
-        legend.position = c(.78,.865),
-        legend.margin = margin(2,0,2,2),
+        legend.position = c(.84,.89),
+        legend.margin = margin(l=.6, r=.6, t=.5, b=.5),
         legend.spacing.y = unit(0, "pt"),
-        legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour ="black"),
-        legend.text = element_text(size=legendsize),
+        legend.spacing.x = unit(0.3, "pt"),
+        legend.background = element_rect(fill="white", size=0.3, linetype="solid", colour ="black"),
         legend.key = element_rect(fill="white"),
-        legend.key.size = unit(2,"line"),
-        legend.key.width = unit(.5, "cm"),
+        legend.text = element_text(size=legendsize),
+        legend.key.height = unit(17, "pt"),
+        legend.key.width = unit(.4, "cm"),
         panel.background = element_rect(fill = "white"),
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        axis.line = element_line(size = 1, linetype = "solid", colour = "black"),
-        plot.title = element_text(hjust=-.3, vjust=2.12, size = 18))
-NKCAL_plot <- NKCAL_plot + guides(colour = guide_legend(override.aes = list(alpha = .7)))
-
-
-
+        axis.line = element_line(size = .3, linetype = "solid", colour = "black"),
+        plot.margin= grid::unit(c(0, 0, 0, 0), "in"),
+        plot.title = element_text(size=generalsize, margin = margin(t = -9, r = 0, b = 0, l = 0), face="bold", vjust = -3.3, hjust = .01))
 
 
 
@@ -105,45 +108,50 @@ NK <- reldata[,c("specimen_ID", "code","nk_relWB")]
 NK$code <- factor(NK$code, levels=c("NewEmerge", "LabReared", "Nesting"))
 
 #Boxplot
-par(mar=c(0,6,4,.5))
+par(mar=c(0,0,0,0))
 NK_plot <- ggplot(NK, aes(x=code, y=nk_relWB, fill=factor(code))) +
-  geom_boxplot(outlier.shape = NA, color="black") +
-  scale_fill_manual(values = c("white", "gray70", "gray38"), labels=c("Newly-emerged", "Lab-reared", "Nesting")) +
-  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=1, seed=333), size=3.1, stroke=.7) +
-  geom_point(aes(color=factor(code)), shape=16, position=position_jitterdodge(jitter.width=1, seed=333), alpha=.7, size=3) +
+  geom_boxplot(outlier.shape = NA, color="black", lwd=.3) +
+  scale_fill_manual(values = c("white", "gray45", "gray20"), labels=c("Newly-emerged", "Lab-reared", "Nesting")) +
+  geom_point(aes(color=factor(code)), shape=16, position=position_jitterdodge(jitter.width=1, seed=333), alpha=.75, size=dotsize) +
+  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=1, seed=333), size=dotsize+.01, stroke=.3) +
   scale_color_manual(values=c("NewEmerge"="mediumblue", "LabReared"="violetred4","Nesting"="darkorange"), labels=c("Newly-emerged", "Lab-reared", "Nesting")) +
   
   #Significance annotations from post-hoc results
   annotate(geom="text", x=1, y=(max(NK[NK$code=="NewEmerge", 3]) *.75)+.07, label="a", color="black", size=lettersize) + #NE
   annotate(geom="text", x=2, y=max(NK[NK$code=="LabReared", 3]) +.1, label="a", color="black", size=lettersize) + #LR
   annotate(geom="text", x=3, y=(max(NK[NK$code=="Nesting", 3])*.87) +.11, label="b", color="black", size=lettersize) + #REP
-  ggtitle("(b)") +
+  ggtitle("B") +
   scale_y_continuous(name = "Ratio of Neuropil to Kenyon Cell Volume", limits = c(2.0,4.5), breaks=seq(2.0,4.5, 0.5)) +
   scale_x_discrete(name = "", labels = c("NE\n","LR","NS")) +
-  theme(text = element_text(color="black", size = generalsize),
-        axis.title = element_text(color="black"), 
-        axis.text.x = element_text(color="black", size = generalsize),
-        axis.text.y = element_text(color="black", size = generalsize, margin = margin(l = 10, r=5)),
-        axis.ticks.margin=unit(0,'cm'),
+  theme(text = element_text(color="black", size = generalsize, family="sans"),
+        axis.title = element_text(color="black", size=9), 
+        axis.text.x = element_text(color="black", size = wordsize, margin = margin(l=0, r=0, t=2, b=-10)), 
+        axis.text.y = element_text(color="black", size = wordsize, margin = margin(l = 2, r=2)),
         axis.ticks.length = unit (.2,"cm"),
+        axis.ticks = element_line(color="black", size=.3),
         legend.position = "none",
         panel.background = element_rect(fill = "white"),
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        axis.line = element_line(size = 1, linetype = "solid",colour = "black"),
-        plot.title = element_text(hjust=-.7, vjust=2.12, size = 18))
+        axis.line = element_line(size = .3, linetype = "solid", colour = "black"),
+        plot.margin= grid::unit(c(0, 0, 0, 0), "in"),
+        plot.title = element_text(size = generalsize, margin = margin(t = -9, r = 0, b = 0, l = 0), face="bold", vjust = -3.3, hjust = .03))
 
 
-multipanel_exp1largestructures <- plot_grid(NKCAL_plot, NULL, NK_plot, rel_widths = c(.65,.03,.35), nrow = 1) #null adds more space between
 
-jpeg("NmelExp1_bigstructures.jpeg", height=6, width = 8, units = "in", res = 500)
+multipanel_exp1largestructures <- plot_grid(NKCAL_plot, NULL, NK_plot, rel_widths = c(.78,.05,.32), nrow = 1) #null adds more space between
 
-multipanel_exp1largestructures
-
-dev.off()
-
-
+ggsave("Figure2.pdf",
+       plot = last_plot(),
+       device = "pdf",
+       path = "D:/USUFiles_02072020/Documents/PHD/Manuscripts/Nomia_Age-Experience_20162018/Submissions/JournalofExperimentalBiology/Figures",
+       scale = 1,
+       width = 5,
+       height = 3.5,
+       units = "in",
+       dpi = 300,
+       limitsize = TRUE)
 
 
 
@@ -165,12 +173,12 @@ melt_MB_neuropil$code <- factor(melt_MB_neuropil$code, levels=c("NewEmerge", "La
 
 
 ##plot
-par(mar=c(0,2,2,.5)) #(bottom, left, top, right)
+par(mar=c(0,0,0,0)) #(bottom, left, top, right)
 MBNeuropil_plot <- ggplot(melt_MB_neuropil, aes(x=variable, y=value, fill=factor(code))) +
-  geom_boxplot(outlier.shape = NA, color="black") +
-  scale_fill_manual(values = c("white", "gray70", "gray38"), labels=c("Newly-emerged", "Lab-reared", "Nesting")) +
-  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.2, seed=12531), size=3.1, stroke=.7) +
-  geom_point(aes(color = factor(code)), pch=16, position=position_jitterdodge(jitter.width=.2, seed=12531), alpha=.7, size=3) +
+  geom_boxplot(outlier.shape = NA, color="black", lwd=.3) +
+  scale_fill_manual(values = c("white", "gray45", "gray20"), labels=c("Newly-emerged", "Lab-reared", "Nesting")) +
+  geom_point(aes(color = factor(code)), pch=16, position=position_jitterdodge(jitter.width=.2, seed=12531), alpha=.75, size=dotsize) +
+  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.2, seed=12531), size=dotsize+.01, stroke=.3) +
   scale_color_manual(values=c("NewEmerge"="mediumblue", "LabReared"="violetred4","Nesting"="darkorange"), labels=c("Newly-emerged", "Lab-reared", "Nesting")) +
   
   #Significance annotations from post-hoc results
@@ -197,34 +205,38 @@ MBNeuropil_plot <- ggplot(melt_MB_neuropil, aes(x=variable, y=value, fill=factor
   scale_y_continuous(name = "Volume Relative to Whole Brain", limits = c(0.02,.12), breaks=seq(0.02,.12, .02)) +
   scale_x_discrete(name = "", labels = c("Lips", "Collars", "Calyces", "MB Lobes")) +
   theme(text = element_text(color="black", size = generalsize),
-        axis.title = element_text(color="black"), 
-        axis.text.x = element_text(color="black", size = generalsize),
-        axis.text.y = element_text(color="black", size = generalsize, margin = margin(l = 10, r=5)),
-        axis.ticks.margin=unit(0,'cm'),
+        axis.title = element_text(color="black", size=9), 
+        axis.text.x = element_text(color="black", size = wordsize, margin = margin(l=0, r=0, t=2, b=-10)), 
+        axis.text.y = element_text(color="black", size = wordsize, margin = margin(l = 2, r=2)),
         axis.ticks.length = unit (.2,"cm"),
+        axis.ticks = element_line(color="black", size=0.3),
         legend.title = element_blank(),
-        legend.position = c(.865,.875),
-        legend.margin = margin(2,2,2,2),
+        legend.position = c(.87,.885),
+        legend.margin = margin(l=.6, r=.6, t=.5, b=.5),
         legend.spacing.y = unit(0, "pt"),
-        legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour ="black"),
+        legend.spacing.x = unit(0.3, "pt"), # reduce distance between key and text
+        legend.background = element_rect(fill="white", size=0.25, linetype="solid", colour ="black"),
         legend.key = element_rect(fill="white"),
         legend.text = element_text(size=legendsize),
-        legend.key.size = unit(2,"line"),
-        legend.key.width = unit(.5, "cm"),
+        legend.key.height = unit(17, "pt"),
+        legend.key.width = unit(.4, "cm"),
         panel.background = element_rect(fill = "white"),
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        axis.line = element_line(size = 1, linetype = "solid",colour = "black"),
-        plot.title = element_text(hjust=0))
+        plot.margin= grid::unit(c(0, 0, 0, 0), "in"),
+        axis.line = element_line(size = .3, linetype = "solid", colour = "black"))
 
-MBNeuropil_plot <- MBNeuropil_plot + guides(colour = guide_legend(override.aes = list(alpha = .7)))
-
-jpeg("NmelExp1_mbneuropil.jpeg", height=5.5, width = 7, units = "in", res = 500)
-
-MBNeuropil_plot
-
-dev.off()
+ggsave("Figure3.pdf",
+       plot = last_plot(),
+       device = "pdf",
+       path = "D:/USUFiles_02072020/Documents/PHD/Manuscripts/Nomia_Age-Experience_20162018/Submissions/JournalofExperimentalBiology/Figures",
+       scale = 1,
+       width = 4,
+       height = 3.5,
+       units = "in",
+       dpi = 300,
+       limitsize = TRUE)
 
 
 
--- Nmel_Experiment2Figures.R
+++ Nmel_Experiment2Figures.R
@@ -1,6 +1,6 @@
 ##Figures for N.melanderi Neuroplasticity; Social Experience 
 ##MA Hagadorn
-##Last Modified Images: 9/20/2020
+##Last Modified Images: 12/15/2020
 ##Last Modified Text: 9/24/2020
 ##Data Used: Nmel_socialbrain2018_structurevolume_proportiondata.csv; generated as a csv output while running Nmel_Experiment2_DataAnalysis
 
@@ -36,44 +36,52 @@ melt_NKCAL$code <- factor(melt_NKCAL$code, levels=c("solo", "paired"))
 
 
 #Assigning specific sizes, changes to figures will reflect changes made here. Keep consistent with Exp.1 figures
-generalsize <- 18
-legendsize <- 13
-lettersize <- 5.2
+generalsize <- 12
+legendsize <- 7
+wordsize <- 8
+lettersize <- 3
+dotsize <- 1.8
+
 
 
 
 #GENERATE FIGURE FOR NEUROPIL, KCS, AND ALS
-par(mar=c(0,2,4,6)) #(bottom, left, top, right)
+par(mar=c(0,0,0,0)) #(bottom, left, top, right)
 NKCAL_plot <- ggplot(melt_NKCAL, aes(x=variable, y=value, fill=factor(code))) +
-  geom_boxplot(outlier.shape = NA, color="black") +
-  scale_fill_manual(values = c("white", "gray70"), labels=c("Solo", "Paired")) +
-  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.35, seed=246), size=3.1, stroke=.7) +
-  geom_point(aes(color = factor(code)), pch=16, position=position_jitterdodge(jitter.width=.35, seed=246), alpha=.7, size=3) +
-  ggtitle("(a)") +
+  geom_boxplot(outlier.shape = NA, color="black", lwd=0.3) +
+  scale_fill_manual(values = c("white", "gray45"), labels=c("Solo", "Paired")) +
+  geom_point(aes(color = factor(code)), pch=16, position=position_jitterdodge(jitter.width=.35, seed=246), alpha=.75, size=dotsize) +
+  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.35, seed=246), size=dotsize+.01, stroke=.3) +
+  ggtitle("A") +
   scale_color_manual(values=c("solo"="forestgreen", "paired"="purple3"), labels=c("Solo", "Paired")) +
   scale_y_continuous(name = "Volume Relative to Whole Brain", limits = c(0.02,.18), breaks=seq(0.02,.18, .04)) +
   scale_x_discrete(name = "", labels = c("MB\nNeuropil", "Kenyon\nCells", "Antennal\nLobes")) +
-  theme(text = element_text(color="black", size = generalsize),
-        axis.title = element_text(color="black"), 
-        axis.text.x = element_text(color="black", size = generalsize),
-        axis.text.y = element_text(color="black", size = generalsize, margin = margin(l = 10, r=5)),
+  theme(text = element_text(color="black", size = generalsize, family="sans"),
+        axis.title = element_text(color="black", size=9), 
+        axis.text.x = element_text(color="black", size = wordsize, margin = margin(l=0, r=0, t=2, b=-10)), 
+        axis.text.y = element_text(color="black", size = wordsize, margin = margin(l = 2, r=2)),
         axis.ticks.margin=unit(0,'cm'),
         axis.ticks.length = unit (.2,"cm"),
+        axis.ticks = element_line(color="black", size = 0.3),
         legend.title = element_blank(),
-        legend.position = c(.88,.94),
-        legend.margin = margin(2,2,2,2),
+        legend.position = c(.90,.925),
+        legend.margin = margin(l=.6, r=.6, t=.5, b=.5),
         legend.spacing.y = unit(0, "pt"),
-        legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour ="black"),
-        legend.text = element_text(size=legendsize),
-        panel.background = element_rect(fill = "white"),
+        legend.spacing.x = unit(0.3, "pt"),
+        legend.background = element_rect(fill="white", size=0.25, linetype="solid", colour ="black"),
         legend.key = element_rect(fill="white"),
+        legend.text = element_text(size=legendsize),
+        legend.key.height = unit(17, "pt"),
+        legend.key.width = unit(.4, "cm"),
+        panel.background = element_rect(fill = "white"),
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        axis.line = element_line(size = 1, linetype = "solid", colour = "black"),
-        plot.title = element_text(hjust=-.32, vjust=2.12, size = generalsize))
+        axis.line = element_line(size = 0.3, linetype = "solid", colour = "black"),
+        plot.margin= grid::unit(c(0, 0, 0, 0), "in"),
+        plot.title = element_text(size = generalsize, margin = margin(t = -9, r = 0, b = 0, l = 0), face="bold", vjust = -3.3, hjust = .01))
 
-NKCAL_plot <- NKCAL_plot + guides(colour = guide_legend(override.aes = list(alpha = .7)))
+NKCAL_plot <- NKCAL_plot + guides(colour = guide_legend(override.aes = list(alpha = .75)))
 
 
 
@@ -90,42 +98,44 @@ NK$code <- factor(NK$code, levels=c("solo", "paired"))
 
 
 #Boxplot
-par(mar=c(0,6,4,.5)) #(bottom, left, top, right)
+par(mar=c(0,0,0,0)) #(bottom, left, top, right)
 NK_plot <- ggplot(NK, aes(x=code, y=nk_relWB, fill=factor(code))) +
-  geom_boxplot(outlier.shape = NA, color="black") +
-  scale_fill_manual(values = c("white", "gray70"), labels=c("Solo", "Paired")) +
-  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=1.1, seed=1), size=3.1, stroke=.7) +
-  geom_point(aes(color = factor(code)), pch=16, position=position_jitterdodge(jitter.width=1.1, seed=1), alpha=.7, size=3) +
-  ggtitle("(b)") +
+  geom_boxplot(outlier.shape = NA, color="black", lwd=.3) +
+  scale_fill_manual(values = c("white", "gray45"), labels=c("Solo", "Paired")) +
+  geom_point(aes(color = factor(code)), pch=16, position=position_jitterdodge(jitter.width=.35, seed=246), alpha=.75, size=dotsize) +
+  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.35, seed=246), size=dotsize+.01, stroke=.3) +
+  ggtitle("B") +
   scale_color_manual(values=c("solo"="forestgreen", "paired"="purple3"), labels=c("Solo", "Paired")) +
   scale_y_continuous(name = "Ratio of Neuropil to Kenyon Cell Volume", limits = c(2.0, 3.5), breaks=seq(2.0, 3.5, 0.5)) +
   scale_x_discrete(name = "", labels = c("Solo\n", "Paired")) +
-  theme(text = element_text(color="black", size = generalsize),
-        axis.title = element_text(color="black"), 
-        axis.text.x = element_text(color="black", size = generalsize),
-        axis.text.y = element_text(color="black", size = generalsize, margin = margin(l = 10, r=5)),
-        axis.ticks.margin=unit(0,'cm'),
+  theme(text = element_text(color="black", size = generalsize, family="sans"),
+        axis.title = element_text(color="black", size=9), 
+        axis.text.x = element_text(color="black", size = wordsize, margin = margin(l=0, r=0, t=2, b=-10)), 
+        axis.text.y = element_text(color="black", size = wordsize, margin = margin(l = 2, r=2)),
         axis.ticks.length = unit (.2,"cm"),
+        axis.ticks = element_line(color="black", size=.3),
         legend.position = "none",
         panel.background = element_rect(fill = "white"),
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        axis.line = element_line(size = 1, linetype = "solid",colour = "black"),
-        plot.title = element_text(hjust=-.8, vjust=2.12, size = generalsize))
+        axis.line = element_line(size = 0.3, linetype = "solid",colour = "black"),
+        plot.margin= grid::unit(c(0, 0, 0, 0), "in"),
+        plot.title = element_text(size = generalsize, margin = margin(t = -9, r = 0, b = 0, l = 0), face="bold", vjust = -3.3, hjust = .03))
 
-multipanel_exp2largestructures <- plot_grid(NKCAL_plot, NULL, NK_plot, rel_widths = c(.65,.03,.35), nrow = 1)
-
-
-jpeg("NmelExp2_bigstructures.jpeg", height=6, width = 7.5, units = "in", res = 500)
-
-multipanel_exp2largestructures
-
-dev.off()
+multipanel_exp2largestructures <- plot_grid(NKCAL_plot, NULL, NK_plot, rel_widths = c(.78,.05,.32), nrow = 1)
 
 
-
-
+ggsave("Figure4.pdf",
+       plot = last_plot(),
+       device = "pdf",
+       path = "D:/USUFiles_02072020/Documents/PHD/Manuscripts/Nomia_Age-Experience_20162018/Submissions/JournalofExperimentalBiology/Figures",
+       scale = 1,
+       width = 4.5,
+       height = 3.5,
+       units = "in",
+       dpi = 300,
+       limitsize = TRUE)
 
 
 
@@ -145,57 +155,53 @@ melt_MB_neuropil <- melt(MB_neuropil)
 melt_MB_neuropil$code <- factor(melt_MB_neuropil$code, levels=c("solo", "paired"))
 
 
-
-
-
-
 ##plot
-par(mar=c(0,2,2,.5)) #(bottom, left, top, right)
+par(mar=c(0,0,0,0)) #(bottom, left, top, right)
 MBNeuropil_plot <- ggplot(melt_MB_neuropil, aes(x=variable, y=value, fill=factor(code))) +
-  geom_boxplot(outlier.shape = NA, color="black") +
-  scale_fill_manual(values = c("white", "gray70"), labels=c("Solo", "Paired")) +
-  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.35, seed=666), size=3.1, stroke=.7) +
-  geom_point(aes(color = factor(code)), pch=16, position=position_jitterdodge(jitter.width=.35, seed=666), alpha=.7, size=3) +
+  geom_boxplot(outlier.shape = NA, color="black", lwd=.3) +
+  scale_fill_manual(values = c("white", "gray45"), labels=c("Solo", "Paired")) +
+  geom_point(aes(color = factor(code)), pch=16, position=position_jitterdodge(jitter.width=.35, seed=246), alpha=.75, size=dotsize) +
+  geom_point(aes(fill=factor(code)), color="black", shape=1, position=position_jitterdodge(jitter.width=.35, seed=246), size=dotsize+.01, stroke=.3) +
   scale_color_manual(values=c("solo"="forestgreen", "paired"="purple3"), labels=c("Solo", "Paired")) +
   
   #<0.001==***, <0.01==**, <0.05=*
   #LIPS
-  annotate(geom="text", x=1, y=max(MB_neuropil[MB_neuropil$code=="paired", 3]) + .0035, label="**", color="black", size = lettersize*2) + #paired
-  
+  annotate(geom="text", x=1, y=max(MB_neuropil[MB_neuropil$code=="paired", 3]) + .0035, label="**", color="black", size = lettersize*1.75) + #paired
   scale_y_continuous(name = "Volume Relative to Whole Brain", limits = c(0.02,.12), breaks=seq(0.02,.12, .02)) +
   scale_x_discrete(name = "", labels = c("Lips", "Collars", "Calyces", "MB Lobes")) +
   theme(text = element_text(color="black", size = generalsize),
-        axis.title = element_text(color="black"), 
-        axis.text.x = element_text(color="black", size = generalsize), 
-        axis.text.y = element_text(color="black", size = generalsize, margin = margin(l = 10, r=5)),
-        axis.ticks.margin=unit(0,'cm'),
+        axis.title = element_text(color="black", size=9), 
+        axis.text.x = element_text(color="black", size = wordsize, margin = margin(l=0, r=0, t=2, b=-10)), 
+        axis.text.y = element_text(color="black", size = wordsize, margin = margin(l = 2, r=2)),
         axis.ticks.length = unit (.2,"cm"),
+        axis.ticks = element_line(color="black", size=.3),
         legend.title = element_blank(),
-        legend.position = c(.91,.91),
-        legend.margin = margin(2,2,2,2),
+        legend.position = c(.92,.92),
+        legend.margin = margin(l=.6, r=.6, t=.5, b=.5),
         legend.spacing.y = unit(0, "pt"),
-        legend.background = element_rect(fill="white", size=0.5, linetype="solid", colour ="black"),
+        legend.spacing.x = unit(0.3, "pt"),
+        legend.background = element_rect(fill="white", size=0.25, linetype="solid", colour ="black"),
         legend.key = element_rect(fill="white"),
         legend.text = element_text(size=legendsize),
-        legend.key.size = unit(2,"line"),
-        legend.key.width = unit(.5, "cm"),
+        legend.key.height = unit(17, "pt"),
+        legend.key.width = unit(.4, "cm"),
         panel.background = element_rect(fill = "white"),
         panel.border = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        axis.line = element_line(size = 1, linetype = "solid",colour = "black"),
-        plot.title = element_text(hjust=0))
-
-MBNeuropil_plot <- MBNeuropil_plot + guides(colour = guide_legend(override.aes = list(alpha = 1)))
+        plot.margin= grid::unit(c(0, 0, 0, 0), "in"),
+        axis.line = element_line(size = .3, linetype = "solid",colour = "black"))
 
 
 
-jpeg("NmelExp2_mbneuropil.jpeg", height=5.5, width = 6.5, units = "in", res = 500)
-
-MBNeuropil_plot
-
-dev.off()
-
-
-
+ggsave("Figure5.pdf",
+       plot = last_plot(),
+       device = "pdf",
+       path = "D:/USUFiles_02072020/Documents/PHD/Manuscripts/Nomia_Age-Experience_20162018/Submissions/JournalofExperimentalBiology/Figures",
+       scale = 1,
+       width = 3.5,
+       height = 3.5,
+       units = "in",
+       dpi = 300,
+       limitsize = TRUE)
 
